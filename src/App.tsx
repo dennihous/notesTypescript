ac10591681a76076dfbf4a06a3f8fdfe28c6c197
dennis <denhouston1@icloud.com>
2023-02-13 17:34:46 +0000

    Adding new paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Navigate, Route, Routes } from "react-router-dom"
+
 function App() {
-  return <h1>Hi</h1>
+  return <Routes>
+    <Route path="/" element={<h1>Hello</h1>} />
+    <Route path="/new" element={<h1>New</h1>} />
+    <Route path="*" element={<Navigate to="/"/>} />
+  </Routes>
 }
 
 export default App
