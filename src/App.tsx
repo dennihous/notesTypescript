ed30c39c912c8ce4ba5f889eea9ea5c3b2247ef5
dennis <denhouston1@icloud.com>
2023-02-14 13:03:28 +0000

    Redirect to home when wrong id is showm

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { NewNote } from "./NewNote"
 import { useLocalStorage } from "./useLocalStorage"
 import { v4 as uuidV4 } from "uuid"
 import { NoteList } from "./NoteList"
+import { NoteLayout } from "./NoteLayout"
 
 export type Note = {
   id: string
@@ -60,7 +61,7 @@ function App() {
     <Routes>
       <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags} />} />
       <Route path="/new" element={<NewNote onSubmit={ onCreateNote } onAddTag={addTag} availableTags={tags} />} />
-      <Route path="/:id">
+      <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
         <Route index element={<h1>Show</h1>} />
         <Route path="edit" element={<h1>Edit</h1>} />
       </Route>
