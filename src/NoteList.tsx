8aa95e4533e299835b1660f5cd81f4711d69121a
dennis <denhouston1@icloud.com>
2023-02-14 12:36:24 +0000

    Added hover and focus styling module css

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,8 +1,9 @@
-import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { useState, useMemo } from "react";
-import { Tag, Note } from "./App";
+import { Tag} from "./App";
+import styles from "./NoteList.module.css";
 
 type SimplifiedNote = {
   tags: Tag[]
@@ -77,5 +78,9 @@ export function NoteList({ availableTags, notes }: NoteListProp){
 }
 
 function NoteCard({ id, title, tags }: SimplifiedNote){
-  return <h1>Hi</h1>
+  return <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`} >
+    <Card.Body>
+
+    </Card.Body>
+  </Card>
 }
