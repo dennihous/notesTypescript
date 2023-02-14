b5e4cb6868affee779854f93fe3a5bc3e1ee96b4
dennis <denhouston1@icloud.com>
2023-02-14 12:12:37 +0000

    Note cards added

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,18 +1,33 @@
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
-import { useState } from "react";
-import { Tag } from "./App";
+import { useState, useMemo } from "react";
+import { Tag, Note } from "./App";
+
+type SimplifiedNote = {
+  tags: Tag[]
+  title: string
+  id: string
+}
 
 type NoteListProp = {
   availableTags: Tag[]
+  notes: SimplifiedNote[]
 }
 
-export function NoteList({ availableTags }: NoteListProp){
+export function NoteList({ availableTags, notes }: NoteListProp){
   const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+  const [title, setTitle] = useState("")
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter(note => {
+      return (title === "" || note.title.toLowerCase().includes(title.toLowerCase())) 
+      && (selectedTags.length === 0 || selectedTags.every(tag => note.tags.some(noteTag => noteTag.id === tag.id)))
+    })
+  }, [title, selectedTags, notes])
 
   return <>
-  <Row>
+  <Row className="align-items-center mb-4">
     <Col><h1>Notes</h1></Col>
     <Col xs="auto">
       <Stack gap={2} direction="horizontal" >
@@ -28,7 +43,7 @@ export function NoteList({ availableTags }: NoteListProp){
       <Col>
       <Form.Group controlId="title">
         <Form.Label>Title</Form.Label>
-        <Form.Control type="text" />
+        <Form.Control type="text" value={title} onChange={e => setTitle(e.target.value)} />
       </Form.Group>
       </Col>
       <Col>
@@ -51,5 +66,16 @@ export function NoteList({ availableTags }: NoteListProp){
       </Col>
     </Row>
   </Form>
+  <Row xs={1} sm={2} lg={3} lx={4} className="g-3">
+    {filteredNotes.map(note => (
+      <Col key={note.id}>
+        <NoteCard id={note.id} title={note.title} tags={note.tags} />
+      </Col>
+    ))}
+  </Row>
   </>
+}
+
+function NoteCard({ id, title, tags }: SimplifiedNote){
+  return <h1>Hi</h1>
 }
