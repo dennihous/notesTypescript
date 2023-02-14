4a1f2b88c5b232947c819d97afe76203004c3da6
dennis <denhouston1@icloud.com>
2023-02-14 12:46:21 +0000

    Adding body to card

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { useState, useMemo } from "react";
@@ -80,7 +80,16 @@ export function NoteList({ availableTags, notes }: NoteListProp){
 function NoteCard({ id, title, tags }: SimplifiedNote){
   return <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`} >
     <Card.Body>
-
+      <Stack gap={2} className="align-items-center justify-content-center h-100">
+        <span className="fs-5">{title}</span>
+        {tags.length > 0 && (
+          <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
+            {tags.map(tag => (
+              <Badge className="text-truncate" key={tag.id}>{tag.label}</Badge>
+            ))}
+          </Stack>
+        )}
+      </Stack>
     </Card.Body>
   </Card>
 }
